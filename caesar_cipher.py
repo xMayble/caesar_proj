7faeb173a2c70ed4bea12206a17441196de9924b
Mayble <mehburohit@gmail.com>
2023-10-23 02:36:41 -0400

    shift

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,8 +1,9 @@
 import art
 
 print(art.logo)
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 
-            'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+             'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 
+             'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
@@ -13,9 +14,14 @@ def caesar(text, shift, direction):
     if direction == 'decode':
             shift *= -1
     for letter in text:
-        idx = alphabet.index(letter)
-        new_index = idx + shift
-        word += alphabet[new_index]
+        if letter in alphabet:
+            idx = alphabet.index(letter)
+            new_index = idx + shift
+            if new_index > 25:
+                new_index = (new_index % 25)-1
+            word += alphabet[new_index]
+        else:
+             word += letter
     print(f"The {direction}d text is {word}")
 
 caesar(text, shift, direction)
