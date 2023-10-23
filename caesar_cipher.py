5e3bb894f34d3d4417c4a3d6ceb6a7dd8ba03c7b
Mayble <mehburohit@gmail.com>
2023-10-22 20:52:47 -0400

    encryption method complete

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -4,3 +4,13 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
+
+# create func called 'encrypt' that takes 'text' & 'shift' as parameters
+def encrypt(text, shift):
+    encrypted_word = ''
+    for i in range(len(text)):
+        index_Of_text = alphabet.index(text[i])
+        encrypted_word += alphabet[index_Of_text + shift]
+    print(encrypted_word)
+encrypt("hello", 2)
+
