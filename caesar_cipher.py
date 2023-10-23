713d6ed7b26d2e7b73856912f72fa572edf0bfbf
Mayble <mehburohit@gmail.com>
2023-10-23 02:14:52 -0400

    added logo and refactored code

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,3 +1,6 @@
+import art
+
+print(art.logo)
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 
             'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
@@ -7,18 +10,13 @@ shift = int(input("Type the shift number:\n"))
 
 def caesar(text, shift, direction):
     word = ''
+    if direction == 'decode':
+            shift *= -1
     for letter in text:
         idx = alphabet.index(letter)
-        if direction == 'encode':
-            new_index = idx + shift 
-            word += alphabet[new_index]
-        elif direction == 'decode':
-            new_index = idx - shift
-            word += alphabet[new_index]
-    if direction == 'encode':
-        print(f"The encrypted text is: {word}")
-    elif direction == 'decode':
-        print(f"The decrypted text is: {word}")
+        new_index = idx + shift
+        word += alphabet[new_index]
+    print(f"The {direction}d text is {word}")
 
 caesar(text, shift, direction)
 
